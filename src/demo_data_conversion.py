c4b10ee043701241e608f38d49a9f1eec0dcbe79
Miles Erickson <miles.erickson@gmail.com>
2016-04-07 14:03:01 -0700

    Add demo_data_conversion

--- src/demo_data_conversion.py
+++ src/demo_data_conversion.py
@@ -2,6 +2,8 @@ import clean_data_code
 import pymongo
 from pymongo import MongoClient
 import pandas as pd
+import graphlab as gl
+
 
 mcl = MongoClient()
 mdb = mcl['events']
@@ -9,5 +11,6 @@ events_in = mdb['incoming']
 json_input = events_in.find_one()
 df = clean_data_code.mongo_to_df(json_input)
 abt = clean_data_code.transform_df(df)
-
-print abt.info(type)
+model = gl.load_model('../models/btc_priceless_v1.gl')
+sf = gl.SFrame(abt)
+pred = model.predict(sf)
