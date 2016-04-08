2ff183422cae02157638a056ab2fbaa117a9a3fc
Miles Erickson <miles.erickson@gmail.com>
2016-04-07 17:41:11 -0700

    final minimal_app.py

--- src/minimal_app.py
+++ src/minimal_app.py
@@ -23,34 +23,34 @@ def check():
     output.append("</head>")
     output.append("<body style='font-family:sans-serif; background-color: black; color: white'>")
 
-    output.append("<h1 style='color:#ff88aa'>Fraud Dashboard</h1>")
-    output.append("<h2 style='color:#ff88aa'>Summary</h2>")
+    output.append("<h1>Fraud Dashboard</h1>")
+    output.append("<h2>Summary</h2>")
     output.append("<ul>")
     mq = {'_pred': {'$exists':1}}
     num_points = events_in.find(mq).count()
-    output.append("<li style='color:#88ffaa'>Number of data points: {0}</li>".format(num_points))
+    output.append("<li>Number of data points: {0}</li>".format(num_points))
 
     mq = {'_pred': "['True']" }
     num_pred_fraud = events_in.find(mq).count()
-    output.append("<li style='color:#88ffaa'>Number predicted fraud: {0}</li>".format(num_pred_fraud))
+    output.append("<li>Number predicted fraud: {0}</li>".format(num_pred_fraud))
 
     mq = {'_pred': "['False']"}
     num_pred_valid = events_in.find(mq).count()
-    output.append("<li style='color:#88ffaa'>Number predicted valid: {0}</li>".format(num_pred_valid))
+    output.append("<li>Number predicted valid: {0}</li>".format(num_pred_valid))
     output.append("</ul>")
 
-    output.append("<h2 style='color:#ff88aa'>Newest Flagged Events</h2>")
-    output.append("<table style='border-collapse: collapse; border: 1px solid grey'; background-color: #330000>")
-    output.append("<tr><th style='border: 1px solid grey'>Title</th><th style='border: 1px solid grey'>Probability</th></tr>")
+    output.append("<h2>Newest Flagged Events</h2>")
+    output.append("<table style='border-collapse: collapse'")
+    output.append("<tr><th style='background-color:#225; color:#ccc'>Title</th><th style='background-color:#225; color:#ccc'>Probability</th></tr>")
 
     # List potentially fraudulent events
     mq = {'_pred': "['True']" }
     fraud_events = num_pred_fraud = events_in.find(mq).sort('_id', pymongo.DESCENDING)
 
     for event in fraud_events:
-        s = "<tr><td style='border: 1px solid grey'>"
+        s = "<tr><td style='background-color:#ccc; color:#500; border: 1px solid grey'>"
         s += event['name']
-        s += "</td><td style='border: 1px solid grey; color:red'>"
+        s += "</td><td style='background-color:#ccc; border: 1px solid grey; color:#500'>"
         s += event['_prob'][1:5]
         s += "</td></tr>"
         output.append(s)
@@ -65,7 +65,7 @@ def check():
 if __name__ == '__main__':
     # Register for service subscription
     #ip_address = socket.gethostbyname(socket.gethostname())
-    ip_address = "10.5.20.159"
+    ip_address = "10.5.20.159"  #TODO remove hardcoded IP address
 
     # Start Flask app
     app.run(host='0.0.0.0', port=PORT, debug=True)
