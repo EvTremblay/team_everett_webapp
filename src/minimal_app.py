4ca896e477ba229623ba5b21c1ceef14e688b546
Miles Erickson <miles.erickson@gmail.com>
2016-04-07 17:13:20 -0700

    Update minimal_app.py and index.html

--- src/minimal_app.py
+++ src/minimal_app.py
@@ -5,6 +5,7 @@ import socket
 import time
 from datetime import datetime
 from pymongo import MongoClient
+import pymongo
 
 mcl = MongoClient()
 mdb = mcl['events']
@@ -17,36 +18,48 @@ PORT = 18080
 def check():
     output = []
 
+    output.append("<html><head>")
+    output.append("<title>Real-Time Predictions</title>")
+    output.append("</head>")
+    output.append("<body style='font-family:sans-serif; background-color: black; color: white'>")
+
+    output.append("<h1 style='color:#ff88aa'>Fraud Dashboard</h1>")
+    output.append("<h2 style='color:#ff88aa'>Summary</h2>")
+    output.append("<ul>")
     mq = {'_pred': {'$exists':1}}
     num_points = events_in.find(mq).count()
-    output.append("Number of data points: {0}".format(num_points))
+    output.append("<li style='color:#88ffaa'>Number of data points: {0}</li>".format(num_points))
 
     mq = {'_pred': "['True']" }
     num_pred_fraud = events_in.find(mq).count()
-    output.append("Number predicted fraud: {0}".format(num_pred_fraud))
+    output.append("<li style='color:#88ffaa'>Number predicted fraud: {0}</li>".format(num_pred_fraud))
 
     mq = {'_pred': "['False']"}
     num_pred_valid = events_in.find(mq).count()
-    output.append("Number predicted valid: {0}".format(num_pred_valid))
+    output.append("<li style='color:#88ffaa'>Number predicted valid: {0}</li>".format(num_pred_valid))
+    output.append("</ul>")
 
+    output.append("<h2 style='color:#ff88aa'>Newest Flagged Events</h2>")
+    output.append("<table style='border-collapse: collapse; border: 1px solid grey'; background-color: #330000>")
+    output.append("<tr><th style='border: 1px solid grey'>Title</th><th style='border: 1px solid grey'>Probability</th></tr>")
 
-    """if data and timestamps:
-        dt = datetime.fromtimestamp(timestamps[-1])
-        data_time = dt.strftime('%Y-%m-%d %H:%M:%S')
-        line2 = "Latest datapoint received at: {0}".format(data_time)
-        line3 = data[-1]
-        output = "{0}\n\n{1}\n\n{2}".format(line1, line2, line3)
-    else:
-        output = line1
-    """
+    # List potentially fraudulent events
+    mq = {'_pred': "['True']" }
+    fraud_events = num_pred_fraud = events_in.find(mq).sort('_id', pymongo.DESCENDING)
+
+    for event in fraud_events:
+        s = "<tr><td style='border: 1px solid grey'>"
+        s += event['name']
+        s += "</td><td style='border: 1px solid grey; color:red'>"
+        s += event['_prob'][1:5]
+        s += "</td></tr>"
+        output.append(s)
+
+    output.append('</table></body></html>')
 
     output = '\n'.join(output)
-    return output, 200, {'Content-Type': 'text/css; charset=utf-8'}
+    return output, 200, {'Content-Type': 'text/html; charset=utf-8'}
 
-
-def register_subscription(ip, port):
-    registration_data = {'ip': ip, 'port': port}
-    requests.post(REGISTER_URL, data=registration_data)
 
 
 if __name__ == '__main__':
